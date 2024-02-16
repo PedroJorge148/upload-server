6410795a73ce3be310641a7e880068bbbc2876a5
Pedro Jorge <pedrojorge2011@gmail.com>
2024-02-16 17:21:21 -0300

    add cors and static folder

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import fastify from 'fastify'
 import { appRoutes } from './routes/appRoutes'
 import fastifyMultipart from '@fastify/multipart'
+import fastifyCors from '@fastify/cors'
+import fastifyStatic from '@fastify/static'
+import { uploadFolder } from './config/upload'
 
 export const app = fastify()
 
+app.register(fastifyStatic, {
+  root: uploadFolder,
+  prefix: '/tmp/uploads',
+})
+
 app.register(fastifyMultipart)
+app.register(fastifyCors)
 
 app.register(appRoutes)
